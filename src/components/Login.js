6239a65caf7ac0336e27a9e59ac1f4f249fbf818
Tristan Miller <tristanmiller89@gmail.com>
2021-06-16 17:20:32 -0500

    added htmlFor and axios package and removed unneeded files

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,61 @@
 import React from 'react';
 import { useState } from 'react';
-
+import axios from 'axios';
 
 const Login = () => {
-    const [form, setForm] = useState({ username: '', password: '', phonenumber: '' });
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    phonenumber: '',
+  });
 
-    const handleChange = (e) => {
-        setForm({
-          ...form,
-          [e.target.name]: e.target.value,
-        });
-      };
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        axios.post('https://reqres.in/', this.state.credentials)
-          .then(res => {
-            localStorage.setItem('token', res.data.payload)
-        })
-          .catch(err => {
-            console.log(err);
-          })
-       
-      };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('https://reqres.in/', this.state.credentials)
+      .then((res) => {
+        localStorage.setItem('token', res.data.payload);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return (
-        <div>
-        <h1>Welcome to Water My Plants!</h1>
-        <div data-testid="loginForm" className="login-form">
-          <form onSubmit={handleSubmit}>
-            <label htmlFor='username'>User Name: </label>
-            <input name="username" value={form.username} onChange={handleChange} />
-            <label>Password: </label>
-            <input name="password" value={form.password} onChange={handleChange} />
-            <label>Phone Number: </label>
-            <input name="phonenumber" value={form.phonenumber} onChange={handleChange} />
-            <button>Submit</button>
-          </form>
-        </div>
-  
-        
+  return (
+    <div>
+      <h1>Welcome to Water My Plants!</h1>
+      <div data-testid="loginForm" className="login-form">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">User Name: </label>
+          <input
+            name="username"
+            value={form.username}
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password: </label>
+          <input
+            name="password"
+            value={form.password}
+            onChange={handleChange}
+          />
+          <label htmlFor="phonenumber">Phone Number: </label>
+          <input
+            name="phonenumber"
+            value={form.phonenumber}
+            onChange={handleChange}
+          />
+          <button>Submit</button>
+        </form>
       </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;

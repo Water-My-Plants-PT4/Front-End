281d6ccdf9059d661be7a4e0429a4e0c11eaca88
Tristan Miller <tristanmiller89@gmail.com>
2021-06-16 19:44:25 -0500

    redux started // redux-thunk package added // index.js wrapped in Provider // store created

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
+import { reducer } from './reducers';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,14 @@
-// putting something in here for the commit
+import moduleName from '../actions';
+
+const initialState = {
+  username: '',
+  password: '',
+  phonenumber: '',
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

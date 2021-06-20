29c22344a14cd43a09714d5a4461922991ca4c7c
TheRealKraytonian <kraytonianu@gmail.com>
2021-06-20 00:19:08 -0700

    user view skeleton complete

--- src/views/user-home/UserHome.js
+++ src/views/user-home/UserHome.js
@@ -1,9 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import PlantCard from "./PlantCard";
 
 export default function Home() {
+  const [plants, setPlants] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://water-my-plants-61621.herokuapp.com/api/plants")
+      .then((res) => {
+        setPlants(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div>
-      <h2>Hello World</h2>
+      <h2>My Plants User Page</h2>
+      <button>Add Plants</button>
+      {plants &&
+        plants.map((plant) => {
+          return <PlantCard details={plant} key={plant.id} />;
+        })}
     </div>
   );
 }
